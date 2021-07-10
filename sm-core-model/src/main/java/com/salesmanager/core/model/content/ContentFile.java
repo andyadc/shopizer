34749aec9e3ef8b132bf469b5f7b77d9d505ffbc
andaicheng <andaicheng@gmail.com>
2021-07-10 21:37:28 +0800

    Polish project

---
+++ sm-core-model/src/main/java/com/salesmanager/core/model/content/ContentFile.java
@@ -0,0 +1,26 @@
+package com.salesmanager.core.model.content;
+
+public abstract class ContentFile {
+
+
+	private String fileName;
+	private String mimeType;
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+
+}
---
+++ sm-core-model/src/main/java/com/salesmanager/core/model/content/ImageContentFile.java
@@ -0,0 +1,13 @@
+package com.salesmanager.core.model.content;
+
+import java.io.Serializable;
+
+public class ImageContentFile extends InputContentFile implements Serializable {
+
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
