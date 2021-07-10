8526b0e5c79fef19a1c12cd7e7b3f9fbefddbfed
andaicheng <andaicheng@gmail.com>
2021-07-10 21:38:24 +0800

    Polish project

---
+++ sm-core-model/src/main/java/com/salesmanager/core/model/shipping/ShippingProduct.java
@@ -0,0 +1,41 @@
+package com.salesmanager.core.model.shipping;
+
+import com.salesmanager.core.model.catalog.product.Product;
+import com.salesmanager.core.model.catalog.product.price.FinalPrice;
+
+public class ShippingProduct {
+
+	private int quantity = 1;
+	private Product product;
+	private FinalPrice finalPrice;
+
+	public ShippingProduct(Product product) {
+		this.product = product;
+
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public FinalPrice getFinalPrice() {
+		return finalPrice;
+	}
+
+	public void setFinalPrice(FinalPrice finalPrice) {
+		this.finalPrice = finalPrice;
+	}
+
+}
