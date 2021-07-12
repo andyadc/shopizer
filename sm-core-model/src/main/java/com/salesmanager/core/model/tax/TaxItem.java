0aa4246015570499601685bb28b0b852e21fb19d
andyadc <andaicheng@gmail.com>
2021-07-12 10:14:01 +0800

    Shopizer

---
+++ sm-core-model/src/main/java/com/salesmanager/core/model/tax/TaxItem.java
@@ -0,0 +1,32 @@
+package com.salesmanager.core.model.tax;
+
+import com.salesmanager.core.model.order.OrderTotalItem;
+import com.salesmanager.core.model.tax.taxrate.TaxRate;
+
+public class TaxItem extends OrderTotalItem {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private String label;
+	private TaxRate taxRate = null;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public TaxRate getTaxRate() {
+		return taxRate;
+	}
+
+	public void setTaxRate(TaxRate taxRate) {
+		this.taxRate = taxRate;
+	}
+
+
+}
